e3e169e25e7bf984ce1d4524ac58944659c9cb69
Jon33260 <j.mallet82@laposte.net>
2024-12-09 12:12:42 +0100

    couleurs

---
+++ script.js
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+
+const lisa = "Lisa";
+const brayan = "Brayan";
+const julian = "Julian";
+const julien = "Julien";
+
+console.log(
+    chalk.red(lisa),
+    chalk.blue(brayan),
+    chalk.green(julian),
+    chalk.yellow(julien)
+);
